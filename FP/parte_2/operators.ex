82e10f841dad29443fc4a1cbfbd3996986e42a88
Esdras Eduardo <esdrasedu@gmail.com>
2015-09-19 10:20:54 -0300

    make module operators, need do doc

--- FP/parte_2/operators.ex
+++ FP/parte_2/operators.ex
@@ -1,7 +1,43 @@
 defmodule Operators do
 
   @doc """
-    TODO principalmente |>
+    Operadores aritméticos, logicos e outros
   """
+
+  def arithmetic_operators do
+    1 + 2 = 3
+    3 - 1 = 2
+    2 * 2 = 4
+    4 / 2 = 2.0
+    div(5, 2) = 2
+    rem(5, 2) = 1
+  end
+
+  def logical_operators do
+    1 == 1.0
+    1 !== 1.0
+    1 === 1
+    1 > 0
+    1 >= 0
+    1 < 2
+    1 <= 2
+    (true && false) == (true and false)
+    (true || false) == (true or false)
+    !false == not false
+  end
+
+  def other_operators do
+    2 |> div(2) = div(2, 2)
+    
+    x = 1
+    try do
+      ^x = 2 
+    rescue 
+      _ -> IO.puts("x cannot be rebinding")
+    end
+    {^x, y} = {1, 2}
+    IO.puts(x)
+    IO.puts(y)
+  end
 
 end
