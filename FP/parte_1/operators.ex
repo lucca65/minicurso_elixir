2a923e199baa2418d991b37a44194fe131b093ca
leorog <leo.rog@gmail.com>
2015-09-20 13:59:53 -0300

    ajuste

--- FP/parte_1/operators.ex
+++ FP/parte_1/operators.ex
@@ -31,11 +31,11 @@ defmodule Operators do
   def other_operators do
     2 |> div(2) = div(2, 2)
 
-    IO.puts([1, 2, 3] ++ [4, 5, 6])
-    IO.puts([1, 2, 3, 4] -- [2, 3])
+    IO.inspect([1, 2, 3] ++ [4, 5, 6])
+    IO.inspect([1, 2, 3, 4] -- [2, 3])
 
     for x <- 1..10, do: IO.puts(x)
-    
+
     x = 1
     try do
       ^x = 2
