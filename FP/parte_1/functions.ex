9ef3693d044e6fd9cc2c335a93531555dd624c8f
leorog <leo.rog@gmail.com>
2015-09-20 13:57:17 -0300

    organizando

--- FP/parte_1/functions.ex
+++ FP/parte_1/functions.ex
@@ -1,14 +1,12 @@
 defmodule Functions do
   @doc """
-   TODO
-
    Declarações normais
-   def
-   defp
+   def -> função visivél fora do módulo
+   defp -> função encapsulada
 
    Declarações anônimas
-   fn() -> ... end
-   &(...)
+   fn() -> ... end -> declaração normal
+   &(...) -> declaração simplificada
   """
 
   def hello do
@@ -23,23 +21,29 @@ defmodule Functions do
 
   def default_value_syntactic_sugar_hello(hello \\ "hello"), do: IO.puts hello
 
-  def run_it do
+  def anonymous_function do
+    # definindo funções anônimas
     anonymous_function = fn -> IO.puts "Hi!" end
     another_anonymous = fn (x) -> IO.puts "Your value: #{x}" end
 
+    # chamando funções anônimas
+    anonymous_function.()
+    another_anonymous.("WOW")
+
+    # mais exemplos
     long_sum = fn(x, y) -> x + y end
     small_sum = &(&1 + &2)
 
     small_print = &(IO.puts(&1))
     smaller_print = &IO.puts/2
 
-    # calling those functions
-    anonymous_function.()
-    another_anonymous.("WOW")
-
     long_sum.(10, 5) |> IO.puts
     small_sum.(10, 5) |> IO.puts
 
     small_print.("Nice!")
+  end
+
+  def try_it(function \\ &(IO.puts "Hello!"))
+    function.()
   end
 end
--- FP/parte_1/variables.ex
+++ FP/parte_1/variables.ex
@@ -8,5 +8,14 @@ defmodule Variables do
 
     rebound
   """
-
+  def variables do
+    x = 1          # integer
+    x = 0x1F       # integer
+    x = 1.0        # float
+    x = true       # boolean
+    x = :atom      # atom / symbol
+    x = "elixir"   # string
+    x = [1, 2, 3]  # list
+    x = {1, 2, 3}  # tuple
+  end
 end
