176966d7bc872f0226aa5ae0f7f5727a32f03279
leorog <leo.rog@gmail.com>
2015-09-21 22:28:28 -0300

    melhorando pattern_mattching e removendo trycaatch

--- FP/parte_2/pattern_matching.ex
+++ FP/parte_2/pattern_matching.ex
@@ -1,21 +1,24 @@
 defmodule PatternMatching do
 
   @doc """
-    TODO
+    Pattern Matching, em programação funcional, é uma forma de obter uma, ou
+    várias, variáveis que estão dentro de uma estrutura complexa (list, tuple,
+    map, dict...) através da definição de um pattern(modelo).
   """
 
   def list_example do
-    lista = String.split "E l I x I r", " "
-    IO.puts "lista = #{lista}"
+    lista = [1,2,3,4,5]
+    IO.inspect lista
 
-    [primeiro | resto] = lista
+    [head | tail] = lista
+    IO.puts "Head:"
+    IO.inspect head
+    IO.puts "Tail:"
+    IO.inspect tail
 
-    IO.puts "Head: #{primeiro}"
-    IO.puts "Tail: #{resto}"
-
-    [primeiro_do_resto | _] = resto
-
-    IO.puts "Tail_Head: #{primeiro_do_resto}"
+    [a,b,c,_,d] = lista
+    IO.puts a + b
+    IO.puts c + d
   end
 
   def tuple_example do
@@ -29,6 +32,23 @@ defmodule PatternMatching do
     IO.puts "valores: #{a} - #{b} - #{c}"
   end
 
+  def map_example do
+    map = %{name: "Charlinho", age: 20}
+    IO.inspect map
+
+    %{name: x, age: _} = map
+    IO.puts x
+
+    %{name: _, age: y} = map
+    IO.puts y
+
+    map_nomes = %{nomes: ["Charlinho", "Paula", "Roberto", "Julia"]}
+    IO.inspect map_nomes
+    %{nomes: [primeiro|[primeiro_do_tail|_]]} = map_nomes
+    IO.puts primeiro
+    IO.puts primeiro_do_tail
+  end
+
   def tuple_case_example do
     tuple = {23, 9, 2015}
     case tuple do
@@ -39,13 +59,13 @@ defmodule PatternMatching do
     end
   end
 
-  def in_function(nil), do: nil #when value is nil
-  def in_function([10|tail]), do: tail # when its a list, and the first element is the number 10
-  def in_function([x|y]), do: y # when calling first element x, and the rest y
+  def in_function(nil), do: nil #quando o valor é nil
+  def in_function([10|tail]), do: tail #quando é uma lista e o primeiro elemento é 10
+  def in_function([x|y]), do: x #retorna o primeiro elemento da lista
   def in_function(<< "At start",  rest::binary >>) do
     "Without start: #{rest}"
   end
-  def in_function(_), do: "all the rest"
+  def in_function(_), do: "all the rest" # nenhuma das outras opções
 
   def run_it do
     in_function(nil) |> IO.inspect
