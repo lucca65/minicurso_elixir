4d8ae5460325d3966ed52f0396096fb6e5d66c44
Lucca <julienlucca@gmail.com>
2015-09-23 02:51:01 -0300

    docs!

--- FP/exerc/extra/word_list/pure.exs
+++ FP/exerc/extra/word_list/pure.exs
@@ -5,19 +5,32 @@ defmodule WordList do
   and the number of times each were used
   """
 
+  @doc """
+  read file and make it downcase
+  """
   def read_file do
     {:ok, text} = File.read("vish.txt")
     text |> String.downcase
   end
 
+  @doc """
+  this splits the words and make the list follow
+  alphabetical order
+  """
   def as_ordered_list(text) do
     text |> String.split |> Enum.sort
   end
 
+  @doc """
+  Here we create groups for words that repeat
+  """
   def as_group_list(word_list) do
     Enum.group_by(word_list, &(&1))
   end
 
+  @doc """
+  this generates an tuple with our word, and the number of ocurrencies
+  """
   def to_occurency_tuple(word_list) do
     word_list |> Map.to_list |> Enum.map(fn (x) ->
       {word, oc} = x
@@ -25,10 +38,16 @@ defmodule WordList do
     end)
   end
 
+  @doc """
+  This sort the tuples by ocurrency
+  """
   def as_ordered_tuples(tuples) do
     tuples |> Enum.sort_by(&(elem(&1, 1))) |> Enum.reverse
   end
 
+  @doc """
+  Duh
+  """
   def top10(tuples) do
     tuples |> Enum.take(10)
   end
